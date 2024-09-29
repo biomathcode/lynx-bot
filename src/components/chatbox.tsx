2003e5340df3d67b5c955cb1b4c7454fcd23e45d
Pratik Sharma <sharma.pratik2016@gmail.com>
2024-09-29 18:26:21 +0530

    fix: changed

--- src/components/chatbox.tsx
+++ src/components/chatbox.tsx
@@ -1,15 +1,15 @@
-import BrandBox from "./brand";
 import Button from "./button";
 
 function ChatBox() {
   return (
-    <div className="   flex flex-col gap-2 w-full items-start">
-      <BrandBox />
+    <div className=" sticky -bottom-8 py-2   flex flex-col gap-2 w-full items-start">
+      {/* <BrandBox /> */}
 
       <form className=" w-full  bg-white flex flex-col  gap-4 ">
         <textarea
-          rows={4}
-          className=" line-clamp-4 border border-brand-border rounded-lg w-full "
+          rows={2}
+          placeholder="Type message here"
+          className=" p-4  line-clamp-4 border border-brand-border rounded-lg w-full "
         />
         <div className=" self-end">
           <Button type="secondary">Continue</Button>
--- src/components/personcard.tsx
+++ src/components/personcard.tsx
@@ -2,10 +2,10 @@ import { MicrosoftLogo } from "./icons";
 
 function PersonCard() {
   return (
-    <div className=" border flex flex-col justify-center items-center gap-4 p-4 drop-shadow-brand-card w-[280px] h-[330px] bg-white rounded-sm">
+    <div className=" border flex flex-col justify-center items-center gap-4 p-4 drop-shadow-brand-card w-[200px] h-[240px] bg-white rounded-sm">
       <img
         src="/images/profile.png"
-        className=" w-[242.91px] h-[236.42px] rounded-sm"
+        className="  w-[180px] h-[180px] rounded-sm"
       />
       <MicrosoftLogo />
     </div>
